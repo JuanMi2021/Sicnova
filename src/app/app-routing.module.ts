3ae494325662f3ae9061ea30849325ee3a686983
JuanMi <80775621+JuanMi2021@users.noreply.github.com>
2022-10-05 11:04:47 +0200

    reacion de los  componentes Dashboard, administracion, Iban, Pedidos, DHL, Ceacerro, Facturaci on y Shop. Cracion del componente Navbar y creado el Menu con vision en el componen te de toda la aplicacion en el dashboard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IbanComponent } from './components/administracion/iban/iban.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CeaceroComponent } from './components/pedidos/ceacero/ceacero.component';
+import { DhlComponent } from './components/pedidos/dhl/dhl.component';
+import { ShopComponent } from './components/shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'iban', component: IbanComponent },
+  { path: 'dhl', component: DhlComponent },
+  { path: 'ceacero', component: CeaceroComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'dashboard', component: DashboardComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AdministracionComponent } from './components/administracion/administracion.component';
+import { IbanComponent } from './components/administracion/iban/iban.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { DhlComponent } from './components/pedidos/dhl/dhl.component';
+import { CeaceroComponent } from './components/pedidos/ceacero/ceacero.component';
+import { FacturacionComponent } from './components/facturacion/facturacion.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { NavbarComponent } from './components/dashboard/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AdministracionComponent,
+    IbanComponent,
+    PedidosComponent,
+    DhlComponent,
+    CeaceroComponent,
+    FacturacionComponent,
+    ShopComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
