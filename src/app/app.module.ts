4f8b5102bda2c14795815ea31a83496e3aec9051
JuanMi <80775621+JuanMi2021@users.noreply.github.com>
2022-10-10 15:33:14 +0200

    Punto de control para realizar un pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -56,6 +57,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     RegistroComponent,
     EditarComponent,
     SpinnerComponent,
+
 
   ],
   imports: [
